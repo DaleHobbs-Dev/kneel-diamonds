9ccd8f76962ae4507f18d81be3fe3010be9e0911
Dale Hobbs <dale.hobbs.dev@gmail.com>
2025-09-22 20:34:55 -0500

    added transient state variable and handlers to modify it as well as attached event listeners to trigger the handlers

--- scripts/StyleOptions.js
+++ scripts/StyleOptions.js
@@ -1,22 +1,25 @@
+import { setStyleChoice } from "./TransientState.js";
+
+export const handleStyleChoice = (event) => {
+  setStyleChoice(parseInt(event.target.value))
+}
+
 export const StyleOptions = async () => {
-    const response = await fetch("http://localhost:8088/styles");
-    const styles = await response.json();
+  const response = await fetch("http://localhost:8088/styles");
+  const styles = await response.json();
 
-    debugger;
-    console.log(styles);
-
-    let styleOptionsHTML = `
+  let styleOptionsHTML = `
     <label for="style" class="form-label">Style Options: </label>
   <select id="style" name="style" class="form-select">
   `;
 
-    styleOptionsHTML += `<option value="0">Select a style option</option>`;
+  styleOptionsHTML += `<option value="0">Select a style option</option>`;
 
-    styleOptionsHTML += styles.map(style => {
-        return `<option value="${style.id}">${style.style}</option>`;
-    }).join("");
+  styleOptionsHTML += styles.map(style => {
+    return `<option value="${style.id}">${style.style}</option>`;
+  }).join("");
 
-    styleOptionsHTML += `</select>`;
+  styleOptionsHTML += `</select>`;
 
-    return styleOptionsHTML;
+  return styleOptionsHTML;
 }
--- scripts/main.js
+++ scripts/main.js
@@ -41,6 +41,8 @@ const render = async () => {
     `
 
     container.innerHTML = composedHTML
+
+    import("./eventHub.js")
 }
 
 render()
